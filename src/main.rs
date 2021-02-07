c3ddb16bac4e7ad3e5b485a78159751a2eb4625f
Dan Bruder <danb@structionsite.com>
2021-02-07 10:19:28 -0500

    run and get stats for redis

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,69 @@
+use async_trait::async_trait;
+use futures::StreamExt;
 use shiplift::{Container, ContainerOptions, Docker};
-use std::env;
+use xtra::prelude::*;
+use xtra::spawn::Tokio;
+
+struct Cont {
+    docker: Docker,
+}
+
+impl Cont {
+    fn new(docker: Docker) -> Self {
+        Self { docker }
+    }
+}
+
+impl Actor for Cont {}
+
+struct Run(String);
+impl Message for Run {
+    type Result = String;
+}
+
+#[async_trait]
+impl Handler<Run> for Cont {
+    async fn handle(&mut self, run: Run, _ctx: &mut Context<Self>) -> String {
+        let result = self
+            .docker
+            .containers()
+            .create(&ContainerOptions::builder(run.0.as_ref()).build())
+            .await
+            .unwrap();
+
+        let container = Container::new(&self.docker, result.id.clone());
+        container.start().await.unwrap();
+
+        result.id
+    }
+}
+
+struct Log(String);
+impl Message for Log {
+    type Result = ();
+}
+
+#[async_trait]
+impl Handler<Log> for Cont {
+    async fn handle(&mut self, log: Log, _ctx: &mut Context<Self>) {
+        let containers = self.docker.containers();
+        let container = containers.get(log.0);
+        dbg!("got container");
+        while let Some(result) = container.stats().next().await {
+            match result {
+                Ok(stat) => println!("{:?}", stat),
+                Err(e) => eprintln!("Error: {}", e),
+            }
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let docker = Docker::new();
-    let image = env::args()
-        .nth(1)
-        .expect("You need to specify an image name");
 
-    let result = docker
-        .containers()
-        .create(&ContainerOptions::builder(image.as_ref()).build())
-        .await
-        .unwrap();
-
-    let container = Container::new(&docker, result.id);
-
-    container.start().await.unwrap();
+    let addr = Cont::new(docker).create(None).spawn(&mut Tokio::Global);
+    let id = addr.send(Run("redis".to_string())).await.unwrap();
+    addr.send(Log(id)).await.unwrap();
+    loop {}
 }
